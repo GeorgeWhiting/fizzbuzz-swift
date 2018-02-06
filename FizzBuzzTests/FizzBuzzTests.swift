2905242454d82d4489e613b28bb7f8032cf6712c
George Whiting <george_whiting@hotmail.com>
2018-02-06 14:55:20 +0000

    refactors tests, multiple variables not needed

--- FizzBuzzTests/FizzBuzzTests.swift
+++ FizzBuzzTests/FizzBuzzTests.swift
@@ -3,41 +3,34 @@ import XCTest
 
 class FizzBuzzTests: XCTestCase {
     
-    var nine:FizzBuzz!
-    var four:FizzBuzz!
-    var ten:FizzBuzz!
-    var fifteen:FizzBuzz!
+    var num:FizzBuzz!
     
     override func setUp() {
         super.setUp()
-        nine = FizzBuzz(num: 9)
-        four = FizzBuzz(num: 4)
-        ten = FizzBuzz(num: 10)
-        fifteen = FizzBuzz(num: 15)
     }
     
     override func tearDown() {
         super.tearDown()
-        nine = nil
-        four = nil
-        ten = nil
-        fifteen = nil
     }
     
     func testFizzOutput() {
-        XCTAssertEqual(nine.returnNum(), "Fizz")
+        num = FizzBuzz(num: 9)
+        XCTAssertEqual(num.returnNum(), "Fizz")
     }
     
     func testNormalOutput() {
-        XCTAssertEqual(four.returnNum(), "4")
+         num = FizzBuzz(num: 4)
+        XCTAssertEqual(num.returnNum(), "4")
     }
     
     func testBuzzOutput() {
-        XCTAssertEqual(ten.returnNum(), "Buzz")
+         num = FizzBuzz(num: 10)
+        XCTAssertEqual(num.returnNum(), "Buzz")
     }
     
     func testFizzBuzzOutput() {
-        XCTAssertEqual(fifteen.returnNum(), "FizzBuzz")
+         num = FizzBuzz(num: 15)
+        XCTAssertEqual(num.returnNum(), "FizzBuzz")
     }
     
 }
