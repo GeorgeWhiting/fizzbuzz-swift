3665eee5fb9cce31d456e30e29290ed43aa218a1
George Whiting <george_whiting@hotmail.com>
2018-02-06 14:26:50 +0000

    Initial commit

--- FizzBuzzTests/FizzBuzzTests.swift
+++ FizzBuzzTests/FizzBuzzTests.swift
@@ -1,11 +1,3 @@
-//
-//  FizzBuzzTests.swift
-//  FizzBuzzTests
-//
-//  Created by george whiting on 06/02/2018.
-//  Copyright © 2018 george whiting. All rights reserved.
-//
-
 import XCTest
 @testable import FizzBuzz
 
@@ -18,12 +10,9 @@ class FizzBuzzTests: XCTestCase {
         super.setUp()
         nine = FizzBuzz(num: 9)
         four = FizzBuzz(num: 4)
-        
-        // Put setup code here. This method is called before the invocation of each test method in the class.
     }
     
     override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
         super.tearDown()
         nine = nil
         four = nil
@@ -35,13 +24,6 @@ class FizzBuzzTests: XCTestCase {
     
     func testNormalOutput() {
         XCTAssertEqual(four.returnNum(), "4")
-    }
-    
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
     }
     
 }
